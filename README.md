840a30fc3cecfa6bfe1897a50922abd4608b0ab6
Richard Baxter <richard.baxter@baxterai.com>
2022-11-12 00:22:57 +1100

    add graph RobertaRecursiveTransformer draft

---
+++ README.md
@@ -0,0 +1,1 @@
+![RobertaRecursiveTransformer.png](https://github.com/bairesearch/TSBpt/blob/master/graph/RobertaRecursiveTransformer.png?raw=true)
---
+++ graph/RobertaRecursiveTransformer.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.array([0, 1, 2, 3, 4])
+y1 = np.array([0, 0.4007114856934547, 0.4485383033657074, 0.4697808350682259, np.nan])
+y2 = np.array([0, 0.4195622387599945, 0.45833434972524645, 0.477471009747982, 0.4880324535870552])
+y3 = np.array([0, 0.32205438960790633, np.nan, np.nan, np.nan])
+
+l1, = plt.plot(x, y1, color='r', label='# layers = 6, shared layer params [recursive] (249MB)')
+l2, = plt.plot(x, y2, color='b', label='# layers = 6 (256MB)')
+l3, = plt.plot(x, y3, color='g', label='# layers = 1 (249MB)')
+#plt.plot(x, y1, color='r', label='sharedLayerWeights')
+#plt.plot(x, y2, color='b', label='wosharedLayerWeights')
+plt.xticks(np.arange(min(x), max(x)+1, 1.0))
+
+plt.xlabel("number of oscar en train samples (x1000000)")
+plt.ylabel("test accuracy")
+plt.title("RoBERTa Recursive Transformer (# params normalised)")
+
+plt.legend(handles=[l1, l2, l3])
+
+plt.show()
